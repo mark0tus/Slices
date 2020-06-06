dd7744f4005e488c57a7e0f0efd79fe7d5b3d80f
Amanda <amanda.vanhatalo@tuni.fi>
2020-06-06 17:58:31 +0300

    Made the Menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+ 
+    public void PlayGame () 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // Current Level + 1 (goes to the next level)
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit(); // Closes the game
+    }
+
+}
